0dcc90dc4ce32759a767922815778f5f758f5344
Kulvir Singh <56261797+kulvir06@users.noreply.github.com>
2021-11-24 00:47:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function tatasteel(){
+    window.open("https://www.tatasteel.com/", "_blank");
+}
+
+function wemotive(){
+    window.open("https://wemotiveforge.com/","_blank");
+}
+
+function stc(){
+    window.open("https://stcvit.in/","_blank");
+}
+
+function dbms(){
+    window.open("https://github.com/kulvir06/DBMS-Project","_blank");
+}
+
+function taskmanager(){
+    window.open("https://github.com/kulvir06/trello-like-app","_blank");
+}
+
+function covid(){
+    window.open("https://github.com/kulvir06/Covid-19-STC","_blank");
+}
